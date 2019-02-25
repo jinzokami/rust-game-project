4093d6aceeb93b324e35818fe6d8e5425f98c2b0
Jinzo <gilberto.veras@aol.com>
2019-02-25 09:10:25 -0500

    initial commit

--- src/render_math.rs
+++ src/render_math.rs
@@ -32,7 +32,21 @@ impl Vector2
     pub fn add(&self, other: Vector2) -> Vector2
     {
         Vector2 {
-            elements: [self.elements[0] + other.elements[0]m self.elements[1] + other.elements[1]],
+            elements: [self.elements[0] + other.elements[0], self.elements[1] + other.elements[1]],
+        }
+    }
+
+    pub fn sub(&self, other: Vector2) -> Vector2
+    {
+        Vector2 {
+            elements: [self.elements[0] - other.elements[0], self.elements[1] - other.elements[1]],
+        }
+    }
+
+    pub fn mul(&self, other: f32) -> Vector2
+    {
+        Vector2 {
+            elements: [self.elements[0] * other, self.elements[1] * other],
         }
     }
 }
@@ -75,7 +89,7 @@ impl Vector3
     pub fn add(&self, other: Vector3) -> Vector3
     {
         Vector3 {
-            elements: [self.elements[0] + other.elements[0]m self.elements[1] + other.elements[1], self.elements[2] + other.elements[2]],
+            elements: [self.elements[0] + other.elements[0], self.elements[1] + other.elements[1], self.elements[2] + other.elements[2]],
         }
     }
 }
@@ -118,7 +132,7 @@ impl Vector4
     pub fn add(&self, other: Vector4) -> Vector4
     {
         Vector4 {
-            elements: [self.elements[0] + other.elements[0]m self.elements[1] + other.elements[1], self.elements[2] + other.elements[2], self.elements[3] + other.elements[3]],
+            elements: [self.elements[0] + other.elements[0], self.elements[1] + other.elements[1], self.elements[2] + other.elements[2], self.elements[3] + other.elements[3]],
         }
     }
 }
@@ -129,6 +143,116 @@ impl Index<usize> for Vector4
     fn index(&self, i: usize) -> &f32
     {
         &self.elements[i]
+    }
+}
+
+struct Bivector2
+{
+    elements: f32,
+}
+
+impl Bivector2
+{
+    pub fn new(xy: f32) -> Bivector2
+    {
+        Bivector2 {
+            elements: xy,
+        }
+    }
+
+    pub fn zero() -> Bivector2
+    {
+        Bivector2 {
+            elements: 0,
+        }
+    }
+}
+
+struct Bivector3
+{
+    elements: [f32; 3],
+}
+
+impl Bivector3
+{
+    pub fn new(xy: f32, xz: f32, yz: f32) -> Bivector3
+    {
+        Bivector3 {
+            elements: [xy, xz, yz],
+        }
+    }
+
+    pub fn zero() -> Bivector3
+    {
+        Bivector3 {
+            elements: [0, 0, 0],
+        }
+    }
+}
+
+struct Bivector4
+{
+    elements: [f32; 6],
+}
+
+impl Bivector4
+{
+    pub fn new(xy: f32, xz: f32, xw: f32, yz: f32, yw: f32, zw: f32) -> Bivector4
+    {
+        Bivector4 {
+            elements: [xy, xz, xw, yz, yw, zw],
+        }
+    }
+
+    pub fn zero() -> Bivector4
+    {
+        Bivector4 {
+            elements: [0, 0, 0, 0, 0, 0],
+        }
+    }
+}
+
+struct Trivector3
+{
+    elements: f32,
+}
+
+impl Trivector3
+{
+    pub fn new(xyz: f32) -> Trivector3
+    {
+        Trivector3 {
+            elements: xyz,
+        }
+    }
+
+    pub fn zero() -> Trivector3
+    {
+        Trivector3 {
+            elements: 0,
+        }
+    }
+}
+
+struct Trivector4
+{
+    elements: [f32; 4],
+}
+
+impl Trivector4
+{
+    pub fn new(xyz: f32, xyw: f32, xzw: f32, yzw: f32) -> Trivector4
+    {
+        Trivector3 {
+            elements: [xyz, xyw, xzw, yzw],
+        }
+    }
+
+    pub fn zero() -> Trivector3
+    {
+        Trivector3 {
+            elements: [0, 0, 0, 0],
+        }
     }
 }
 
